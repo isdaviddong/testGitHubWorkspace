6f805d6a8ab3e87b61bf53f4db4ab683e73931c4
David Tung <isdaviddong@gmail.com>
2024-06-25 21:07:37 +0800

    Add BMI calculation feature

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("歡迎使用BMI計算器");
+        Console.Write("請輸入您的體重（公斤）：");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        Console.Write("請輸入您的身高（公分）：");
+        double height = Convert.ToDouble(Console.ReadLine()) / 100; // 身高轉換為米
+        double bmi = CalculateBMI(weight, height);
+        Console.WriteLine($"您的BMI是：{bmi:F2}");
+    }
+
+    static double CalculateBMI(double weight, double height)
+    {
+        return weight / (height * height);
+    }
+}
